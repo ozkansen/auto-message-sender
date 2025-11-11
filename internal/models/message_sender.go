2eda92e5fd991255455392494fe17aede3579bac
Özkan ŞEN <ozkansen@gmail.com>
2025-11-11 22:12:17 +0300

    fix forget sentat field

--- internal/models/message_sender.go
+++ internal/models/message_sender.go
@@ -1,6 +1,11 @@
 package models
 
+import (
+	"time"
+)
+
 type MessageSenderResponse struct {
-	Message   string `json:"message"`
-	MessageID string `json:"message_id"`
+	Message   string    `json:"message"`
+	MessageID string    `json:"message_id"`
+	SentAt    time.Time `json:"sent_at"`
 }
